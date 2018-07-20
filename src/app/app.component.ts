f90bf99ccffcf9febacb8ad47fff1716e208afee
lingling8498 <Ling.Wang@WolfDen.wolfmedical.com>
2018-07-20 16:39:41 -0700

    angular - templateSyntax practice 2018 May 21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Template Syntax - Angular Guide';
+  prefix = 'Mr';
+  public heroName = 'Windstorm';
+  isHidden = true;
+  heroImageUrl = './favicon.ico';
+  evilTitle = 'Template <script>alert("evil never sleeps")</script>Syntax';
+  actionName = "Click Me";
+  public isSpecial = true;
+  public canSave = true;
+  onSaveClickEvent: string;
+  public myEntryPhone;
+  currentHeroBirthdate = "February 25, 1979";
+
+
+
+  @Input()  size: number | string;
+  @Output() sizeChange = new EventEmitter<number>();
+ 
+  dec() { this.resize(-1); }
+  inc() { this.resize(+1); }
+ 
+  resize(delta: number) {
+    this.size = Math.min(40, Math.max(8, +this.size + delta));
+    this.sizeChange.emit(this.size);
+  }
+
+  onSave(){
+    this.onSaveClickEvent = "you click on save button";
+  }
+
+  delete(){
+    console.log("you click delete!");
+  }
+
+  callPhone(phone){
+    this.myEntryPhone = phone;
+    console.log(phone);
+  }
+
+  callFax(fax){
+    console.log(fax);
+  }
 }
